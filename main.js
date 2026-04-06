00271a940b52687d4506d9fe710a35517b3e0a40
guizao1712 <guilherme.leonardi.cruz@escola.pr.gov.br>
2026-04-06 11:44:00 -0300

    ,,,

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const botoes= documentos. querySelectorAll("botao")
 console.log(botoes)
+for (let i=0; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+
+    for(let  j=0; j<botoes.length;j++) {
+        botoes[j].classList.remove("ativo");
+    }   
+        botoes[i].classList.add("ativo");
+    }
+}
